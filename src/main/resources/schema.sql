cea89af62e9e9a84e53c455bfcf4a79f330d9a56
Andrea Fey <andrea.github@mailnull.com>
2014-06-21 18:17:09 -0400

    add client table and person_client relation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,19 @@ CREATE TABLE person (
     state varchar(2) NOT NULL,
     zip_code varchar(5) NOT NULL
 );
+CREATE TABLE client (
+    client_id integer IDENTITY,
+    company_name varchar(50) NOT NULL,
+    uri varchar(50) NOT NULL,
+    phone varchar(14) NOT NULL,
+    street_address varchar(50) NOT NULL,
+    city varchar(50) NOT NULL,
+    state varchar(2) NOT NULL,
+    zip_code varchar(5) NOT NULL
+);
+CREATE TABLE person_client (
+    person_id integer IDENTITY,
+    client_id integer NOT NULL,
+    FOREIGN KEY person_id REFERENCES person(person_id),
+    FOREIGN KEY client_id REFERENCES client(client_id)
+);
